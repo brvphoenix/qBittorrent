df3bc03590b1acabee0b009d8194660aa713d7ab
Chocobo1 <Chocobo1@users.noreply.github.com>
2024-07-23 16:42:17 +0800

    WebUI: avoid excessive checking

--- src/webui/www/private/scripts/piecesbar.js
+++ src/webui/www/private/scripts/piecesbar.js
@@ -90,7 +90,7 @@ window.qBittorrent.PiecesBar = (() => {
             if (vals.width > 0)
                 obj.setPieces(vals.pieces);
             else
-                setTimeout(() => { checkForParent(obj.id); }, 1);
+                setTimeout(() => { checkForParent(obj.id); });
 
             return obj;
         }
@@ -260,7 +260,7 @@ window.qBittorrent.PiecesBar = (() => {
         if (!obj)
             return;
         if (!obj.parentNode)
-            return setTimeout(() => { checkForParent(id); }, 1);
+            return setTimeout(() => { checkForParent(id); }, 100);
 
         obj.refresh();
     }
--- src/webui/www/private/scripts/progressbar.js
+++ src/webui/www/private/scripts/progressbar.js
@@ -106,7 +106,7 @@ window.qBittorrent.ProgressBar = (function() {
             if (vals.width)
                 obj.setValue(vals.value);
             else
-                setTimeout('ProgressBar_checkForParent("' + obj.id + '")', 1);
+                setTimeout('ProgressBar_checkForParent("' + obj.id + '")');
             return obj;
         }
     });
@@ -146,7 +146,7 @@ window.qBittorrent.ProgressBar = (function() {
         if (!obj)
             return;
         if (!obj.parentNode)
-            return setTimeout('ProgressBar_checkForParent("' + id + '")', 1);
+            return setTimeout('ProgressBar_checkForParent("' + id + '")', 100);
         obj.setStyle("width", "100%");
         const w = obj.offsetWidth;
         obj.vals.dark.setStyle("width", w);
